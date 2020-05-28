a3b41aea92314cb5fbbff9db9cf3afe577b7b732
Brogeby <olof.brogeby@cmeducations.se>
2020-05-29 00:12:25 +0200

    cleaning up the adminpage for products

--- ecommerce-template/public/admin/editproducts.php
+++ ecommerce-template/public/admin/editproducts.php
@@ -51,41 +51,28 @@ $msg = '';
 </div>
 
 <section id="admin-list"> 
-    <table>
-        <tr>
-            <th>Img_url</th>
-            <th>Title</th>
-            <th>Brewery</th>
-            <th>Type</th>
-            <th>Price</th>
-            <th>Id</th>
-            <th>description</th>
-            <th></th>
-            <th></th>
-        </tr>
-        <?php foreach ($products as $key => $content) { ?>
-            <tr class="admin-product">
-                <td><img class="admin-image" src="<?=htmlentities(IMG_PATH . $content['img_url'])?>" alt="<?=htmlentities($content['title'])?>"></td>
-                <td class="admin-title"><?=htmlentities($content['title'])?></td>
-                <td class="admin-brewery"><?=htmlentities($content['brewery'])?></td>
-                <td class="admin-type"><?=htmlentities($content['type'])?></td>
-                <td class="admin-price"><?=htmlentities($content['price'])?> sek</td>
-                <td class="admin-id"><?=htmlentities($content['id'])?></td>
-                <td class="admin-desc"><?=htmlentities($content['description'])?></td>
-                <td class="updateDelete">
-                    <form action="#" method="POST">
-                        <input type="hidden" name="deleteId" value="<?=$content['id']?>">
-                        <input type="submit" name="deleteBtn" class="delete-btn" value="Delete product">
-                    </form>
-                    <form action="updateproduct.php" method="GET">
-                        <input type="hidden" name="updateId" value="<?=$content['id']?>">
-                        <input type="submit" name="updateBtn" class="update-btn" value="Update product">
-                    </form>
-                </td>
-            </tr>
-            <hr>
-        <?php } ?>
-    </table>
+    <?php foreach ($products as $key => $content) { ?>
+        <ul class="admin-product">
+            <img class="admin-image" src="<?=htmlentities(IMG_PATH . $content['img_url'])?>" alt="<?=htmlentities($content['title'])?>">
+            <h2 class="admin-title"><?=htmlentities($content['title'])?></h2>
+            <h3 class="admin-brewery"><?=htmlentities($content['brewery'])?></h3>
+            <h3 class="admin-type"><?=htmlentities($content['type'])?></h3>
+            <p class="admin-price"><?=htmlentities($content['price'])?> sek</p>
+            <p class="admin-id"><?=htmlentities($content['id'])?></p>
+            <p class="admin-desc"><?=htmlentities($content['description'])?></p>
+            <div class="updateDelete">
+                <form action="#" method="POST">
+                    <input type="hidden" name="deleteId" value="<?=$content['id']?>">
+                    <input type="submit" name="deleteBtn" class="delete-btn" value="Delete product">
+                </form>
+                <form action="updateproduct.php" method="GET">
+                    <input type="hidden" name="updateId" value="<?=$content['id']?>">
+                    <input type="submit" name="updateBtn" class="update-btn" value="Update product">
+                </form>
+            </div>
+        </ul>
+        <hr>
+    <?php } ?>
 </section>
 
 <?php include '../parts/footer.php';?>
