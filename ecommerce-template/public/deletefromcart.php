0e21be7c4b006cc4fbf9ec532b3974bcb550f3e1
Brogeby <olof.brogeby@cmeducations.se>
2020-06-10 23:53:16 +0200

    lagt till deleteknapp i varukorgen

---
+++ ecommerce-template/public/deletefromcart.php
@@ -0,0 +1,22 @@
+<?php
+ include('../src/config.php');
+ require SRC_PATH . ('dbconnect.php'); // Ger error om filen inte hittas
+ error_reporting(-1);
+
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+    if(!empty($_POST['productId']) 
+        && isset($_SESSION['items'][$_POST['productId']])
+    ) {
+        unset($_SESSION['items'][$_POST['productId']]);
+    }
+
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit;
+
+
+
+
+?>
