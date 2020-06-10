b361a597e6e871642b2951041a8ad0ce257a1a8c
axelsvardh <60424706+axelsvardh@users.noreply.github.com>
2020-06-10 13:10:03 +0200

    meny fix

--- ecommerce-template/public/parts/menu.php
+++ ecommerce-template/public/parts/menu.php
@@ -1,19 +1,51 @@
+<?php
+ //unset($_SESSION['items']);
+if(!isset($_SESSION['items'])) {
+    $_SESSION['items'] = [];
+}
+
+ $productItemCount = count($_SESSION['items']);
+ $productTotalSum = 0; count($_SESSION['items']);
+ foreach ($_SESSION['items'] as $productId => $productItem) {
+      $productTotalSum += $productItem['price'] * $productItem['quantity'];
+ }
+ ?>
  <div id="myNav" class="overlay">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <div class="overlay-content">
-    <a href="index.php">Home</a>
-    <a href="productlist.php">Products</a>
-    <a href="#">Log in</a>
-    <a href="users-public/home-login-reg.php">Log in</a>
+        <a href="<?= APP_URL . 'public/index.php' ?>">Home</a>
+        <a href="<?= APP_URL . 'public/productlist.php' ?>">Products</a>
+        <a href="<?= APP_URL . 'public/index.php' ?>">Gäst</a>
+        <a href="<?= APP_URL . 'public/admin/index.php' ?>">Admin</a>
     </div>
 </div>
-<header>
+<header class="header">
     <h3>Öltanken</h3>
-    <img id="header-logo" src="<?= IMG_PATH . 'bwob-logo.png'?>" alt="Öltanken">
-    <img id="header-cart" src="<?= IMG_PATH . 'business.svg'?>" alt="Shopping Cart">
+    <img id="header-logo" class="header-img" src="<?= IMG_PATH . 'bwob-logo.png'?>" alt="Öltanken">
+    <img id="show-cart" class="header-img" src="<?= IMG_PATH . 'business.svg'?>" alt="Shopping Cart">
+        <div id="cart-modal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h4>Varukorg</h4>
+                <?php foreach ($_SESSION['items'] as $productId => $productItem) { ?>
+                    <div class="cart-details">
+                        <div class="cart-details-img"><img src="<?=IMG_PATH . $productItem['img_url']?>"></div>
+                        <div class="cart-title"><?=$productItem['title']?></div>
+                        <div class="cart-brewery"><?=$productItem['brewery']?></div>
+                        <div class="cart-price"><?=$productItem['price']?>kr</div>
+                        <div class="cart-quantity">I varukorgen: <?=$productItem['quantity']?></div>
+                        <hr>
+                    </div>
+                <?php } ?>
+                <span class="count">Total: <?=$productTotalSum?>kr</span>
+                <br>
+                <a href="checkout.php" class="general-button go-to-checkout">Gå till kassan</a>
+            </div>
+        </div>
     <label class="nav-toggle" for="nav-toggle" onclick="openNav()">
         <span></span>
         <span></span>
         <span></span>
     </label>
 </header>
+<div class="header-margin"></div>
