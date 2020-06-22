0895df5113ddbea58838abded5e7015f24221fb5
Brogeby <olof.brogeby@cmeducations.se>
2020-06-22 16:06:22 +0200

    Påbörjat update refakturering

--- ecommerce-template/public/admin/updateproduct.php
+++ ecommerce-template/public/admin/updateproduct.php
@@ -4,92 +4,90 @@ include('../../src/config.php');
 require SRC_PATH . ('dbconnect.php'); // Ger error om filen inte hittas
 error_reporting(-1);
 
-if(isset($_GET['updateId'])){
-    try {
-      $id = $_GET['updateId'];
-      $query = "SELECT * FROM products
-                WHERE id = :id";
-      $stmt = $dbconnect->prepare($query);
-      $stmt->bindValue(':id', $_GET['updateId']);
-      $stmt->execute();
-      $product = $stmt->fetch();
-    } catch (\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int) $e->getCode());
-    }
-  }
+$product = getUpdateId();
+// if(isset($_GET['updateId'])){
+//     try {
+//       $id = $_GET['updateId'];
+//       $query = "SELECT * FROM products
+//                 WHERE id = :id";
+//       $stmt = $dbconnect->prepare($query);
+//       $stmt->bindValue(':id', $_GET['updateId']);
+//       $stmt->execute();
+//       $product = $stmt->fetch();
+//     } catch (\PDOException $e) {
+//         throw new \PDOException($e->getMessage(), (int) $e->getCode());
+//     }
+// }
 
 
-$img_url = '';
-$title = '';
-$brewery = '';
-$type = '';
-$price = '';
-$description = '';
-$error = '';
-$msg = '';
-if (isset($_POST['updateProductBtn'])) {
-    $img_url = trim($_POST['img_url']);
-    $title = trim($_POST['title']);
-    $brewery = trim($_POST['brewery']);
-    $type = trim($_POST['type']);
-    $price = trim($_POST['price']);
-    $description = trim($_POST['description']);
+// $img_url = '';
+// $title = '';
+// $brewery = '';
+// $type = '';
+// $price = '';
+// $description = '';
+// $error = '';
+// $msg = '';
+// if (isset($_POST['updateProductBtn'])) {
+//     $img_url = trim($_POST['img_url']);
+//     $title = trim($_POST['title']);
+//     $brewery = trim($_POST['brewery']);
+//     $type = trim($_POST['type']);
+//     $price = trim($_POST['price']);
+//     $description = trim($_POST['description']);
 
-    if (empty($title)) {$error .= "<div>Titel får ej vara tom</div>";}
-    if (empty($brewery)) {$error .= "<div>Bryggeri får ej vara tom</div>";}
-    if (empty($type)) {$error .= "<div>Typ får ej vara tom</div>";}
-    if (empty($price)) {$error .= "<div>Pris får ej vara tom</div>";}
-    if (empty($img_url)) {$error .= "<div>img_url får ej vara tom</div>";}
-    if (empty($description)) {$error .= "<div>Beskrivning får ej vara tom</div>";}
-    if ($error) {$msg = "<div class='errors'>{$error}</div>";}
+//     if (empty($title)) {$error .= "<div>Titel får ej vara tom</div>";}
+//     if (empty($brewery)) {$error .= "<div>Bryggeri får ej vara tom</div>";}
+//     if (empty($type)) {$error .= "<div>Typ får ej vara tom</div>";}
+//     if (empty($price)) {$error .= "<div>Pris får ej vara tom</div>";}
+//     if (empty($img_url)) {$error .= "<div>img_url får ej vara tom</div>";}
+//     if (empty($description)) {$error .= "<div>Beskrivning får ej vara tom</div>";}
+//     if ($error) {$msg = "<div class='errors'>{$error}</div>";}
 
-    if (empty($error)) {
-        try {
-            $query = "
-            UPDATE products
-            SET 
-                title = :title, 
-                brewery = :brewery, 
-                type = :type, 
-                price = :price, 
-                img_url = :img_url, 
-                description = :description
-            WHERE id = :id
-            ";
+//     if (empty($error)) {
+//         try {
+//             $query = "
+//             UPDATE products
+//             SET 
+//                 title = :title, 
+//                 brewery = :brewery, 
+//                 type = :type, 
+//                 price = :price, 
+//                 img_url = :img_url, 
+//                 description = :description
+//             WHERE id = :id
+//             ";
 
-            $stmt = $dbconnect->prepare($query);
-            $stmt->bindValue(':img_url', $img_url);
-            $stmt->bindValue(':title', $title);
-            $stmt->bindValue(':brewery', $brewery);
-            $stmt->bindValue(':type', $type);
-            $stmt->bindValue(':price', $price);
-            $stmt->bindValue(':description', $description);
-            $stmt->bindValue(':id', $_GET['updateId']);
-            $result = $stmt->execute();
-        }   catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int) $e->getCode()); 
-            }
-        if ($result) {
-        $msg = '<div class="success">Produkten har uppdaterats!</div>';
-        if(isset($_GET['updateId'])){
-            try {
-              $id = $_GET['updateId'];
-              $query = "SELECT * FROM products
-                        WHERE id = :id";
-              $stmt = $dbconnect->prepare($query);
-              $stmt->bindValue(':id', $_GET['updateId']);
-              $stmt->execute();
-              $product = $stmt->fetch();
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int) $e->getCode());
-            }
-          }
-        } 
-    }
-}
-
-
-
+//             $stmt = $dbconnect->prepare($query);
+//             $stmt->bindValue(':img_url', $img_url);
+//             $stmt->bindValue(':title', $title);
+//             $stmt->bindValue(':brewery', $brewery);
+//             $stmt->bindValue(':type', $type);
+//             $stmt->bindValue(':price', $price);
+//             $stmt->bindValue(':description', $description);
+//             $stmt->bindValue(':id', $_GET['updateId']);
+//             $result = $stmt->execute();
+//         }   catch (\PDOException $e) {
+//             throw new \PDOException($e->getMessage(), (int) $e->getCode()); 
+//         }
+//         if ($result) {
+//         $msg = '<div class="success">Produkten har uppdaterats!</div>';
+//             if(isset($_GET['updateId'])){
+//                 try {
+//                 $id = $_GET['updateId'];
+//                 $query = "SELECT * FROM products
+//                             WHERE id = :id";
+//                 $stmt = $dbconnect->prepare($query);
+//                 $stmt->bindValue(':id', $_GET['updateId']);
+//                 $stmt->execute();
+//                 $product = $stmt->fetch();
+//                 } catch (\PDOException $e) {
+//                     throw new \PDOException($e->getMessage(), (int) $e->getCode());
+//                 }
+//             }
+//         } 
+//     }
+// }
         
 ?>
 <!DOCTYPE html>
